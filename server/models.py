73766898e0bdb1c0496f8b3819d1ead80b4ce767
Shadrack <skmutai6@gmail.com>
2024-07-01 15:27:09 +0300

    commit

--- server/models.py
+++ server/models.py
@@ -3,28 +3,29 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
 
-
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
     reviews = db.relationship('Review', back_populates=('customer'))
 
+    items = association_proxy('reviews', 'item', creator=lambda item_obj: Review(item=item_obj))
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
-
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+    serialize_rules = ('-reviews.item',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -35,8 +36,9 @@ class Item(db.Model):
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
+    serialize_rules = ('-customer.reviews', '-item.reviews',)
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
